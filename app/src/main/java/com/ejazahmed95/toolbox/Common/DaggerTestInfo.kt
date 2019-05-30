56bdebaef26ed92011b52ae16156662b843149a9
Ejaz Ahmed <ejazahmed0195@gmail.com>
2019-05-30 23:29:06 +0530

    integrating barcode scanner

--- app/src/main/java/com/ejazahmed95/toolbox/Common/DaggerTestInfo.kt
+++ app/src/main/java/com/ejazahmed95/toolbox/Common/DaggerTestInfo.kt
@@ -3,5 +3,5 @@ package com.ejazahmed95.toolbox.Common
 import javax.inject.Inject
 
 class DaggerTestInfo @Inject constructor() {
-    val text = "Hello there"
+    val text = "Hello there dagger"
 }
--- app/src/main/java/com/ejazahmed95/toolbox/View/MainActivity.kt
+++ app/src/main/java/com/ejazahmed95/toolbox/View/MainActivity.kt
@@ -7,14 +7,45 @@ import com.ejazahmed95.toolbox.R
 import com.ejazahmed95.toolbox.dagger.components.DaggerAppComponent
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
+import android.content.Intent
+import android.app.Activity
+import android.util.Log
+//import com.google.zxing.integration.android.IntentIntegrator
+
 
 class MainActivity : AppCompatActivity() {
 
     @Inject lateinit var info: DaggerTestInfo
+
+//    lateinit var barScan: IntentIntegrator
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         DaggerAppComponent.create().poke(this)
         text_view.text = info.text
+//        barScan = IntentIntegrator(this)
+        scanBarcode.setOnClickListener {
+            startBarcodeScanner()
+        }
+    }
+
+    private fun startBarcodeScanner() {
+//        val intent = Intent("com.google.zxing.client.android.SCAN")
+//        startActivityForResult(intent, 0);
+//        barScan.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.d("Scanner", "RequestedCode::$requestCode")
+        if(requestCode == 0) {
+            if (resultCode == Activity.RESULT_OK) {
+                // contents contains whatever was encoded
+                val contents = intent.getStringExtra("SCAN_RESULT")
+
+                // Format contains the type of code i.e. UPC, EAN, QRCode etc...
+                val format = intent.getStringExtra("SCAN_RESULT_FORMAT")
+                Log.d("Scanner", "Contents::$contents and format::$format")
+            }
+        }
     }
 }
